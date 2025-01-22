7c8da413b56d8683658baa2656391c19d3cbb0cb
dagash007 <37184758+dagash007@users.noreply.github.com>
2025-01-22 05:45:29 -0500

    Add files via upload

--- Pizza_Sales_Queries.sql
+++ Pizza_Sales_Queries.sql
@@ -1,1 +1,14 @@
+select * from pizza_sales_
+
 SELECT FORMAT(SUM(total_price), 'N0') AS Total_Revenue FROM pizza_sales_;
+
+SELECT FORMAT(SUM(total_price)/COUNT(DISTINCT(order_id)), 'N0') AS Average_Order_Value FROM pizza_sales_;
+
+SELECT FORMAT(SUM(quantity), 'N0') AS Total_Pizza_Sold FROM pizza_sales_;
+
+SELECT FORMAT(COUNT(DISTINCT(order_id)), 'N0') AS Total_Orders FROM pizza_sales_;
+
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2)) AS AVG_Pizzas_Per_Order FROM pizza_sales_
+
+
